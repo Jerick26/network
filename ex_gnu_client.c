4abdae05d9548401b15b939c04e83a836340c0ff
Jerick26 <jerry.pin26@yahoo.com>
2017-06-18 17:15:31 +0800

    Update ex_gnu_client.c

--- ex_gnu_client.c
+++ ex_gnu_client.c
@@ -14,6 +14,24 @@
 #define SERVERHOST      "www.gnu.org"
 
 void
+init_sockaddr (struct sockaddr_in *name,
+               const char *hostname,
+               uint16_t port)
+{
+  struct hostent *hostinfo;
+
+  name->sin_family = AF_INET;
+  name->sin_port = htons (port);
+  hostinfo = gethostbyname (hostname);
+  if (hostinfo == NULL)
+    {
+      fprintf (stderr, "Unknown host %s.\n", hostname);
+      exit (EXIT_FAILURE);
+    }
+  name->sin_addr = *(struct in_addr *) hostinfo->h_addr;
+}
+
+void
 write_to_server (int filedes)
 {
   int nbytes;
@@ -25,7 +43,6 @@ write_to_server (int filedes)
       exit (EXIT_FAILURE);
     }
 }
-
 
 int
 main (void)
